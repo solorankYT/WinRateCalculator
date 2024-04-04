6e4f8712f743d38911ed1e5f4174ad73d7e0b442
markFuna <markfuna6@gmail.com>
2024-04-05 01:59:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ function WinrateCalculator() {
     let winrate = document.getElementById("winrate").value;
     let goal = document.getElementById("goal").value;
     
-    let need = (matches * goal - winrate * 100) / (100 - goal);
+   // let need = (matches * goal - winrate * 100) / (100 - goal);
+
+    let need = (matches*(goal/winrate)) - matches;
+    
 
 
 
-
-
-    document.getElementById("Answer").innerHTML = "Win Needs: " + need;
+    document.getElementById("Answer").innerHTML = "Win Needs: " + need.toFixed(0);
       return false; 
 }
